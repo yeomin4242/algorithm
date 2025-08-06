891d567320b3f6eb679651b12f53f31b8d364ad3
jihea <bictiger3@gamil.com>
2025-08-07 00:00:20 +0900

    BOJ 1085,3009,15894,27323 문제 해결

---
+++ wanghoreng/15894/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        long length = 4*n;
+
+        System.out.println(length);
+    }
+}
